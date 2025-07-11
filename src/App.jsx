f27d9a8f4227bf761e918c43fee745b7f021074c
Kevin Wang <165529597+24kewang@users.noreply.github.com>
2025-07-11 15:29:01 -0400

    Set up TailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,27 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <div className="flex items-center justify-center gap-8">
+        <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
+          <img src={viteLogo} className="logo blur-sm" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+          <img src={reactLogo} className="logo react animate-spin" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className="text-center">Vite + React</h1>
+      <div className="card flex flex-col items-center">
+        <button
+          className="px-4 py-2 mt-4 mb-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs text-center">
         Click on the Vite and React logos to learn more
       </p>
     </>
